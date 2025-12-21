7f396404db9380372a82122f55e7e209184d9e4e
JoseChanP <45737807+JoseChanP@users.noreply.github.com>
2025-12-21 10:01:40 +0000

    Deploying to gh-pages from @ JoseChanP/JoseChanP.github.io@8535ae621810cf6129c8ff92e3a4caf61c0dae4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,11 +451,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-autonomous-donkey-car",
+          title: 'Autonomous Donkey Car',
+          description: "end-to-end navigation on Raspberry Pi",
           section: "Projects",handler: () => {
-              window.location.href = "/al-folio/projects/1_project/";
+              window.location.href = "/al-folio/projects/1_robocar/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
