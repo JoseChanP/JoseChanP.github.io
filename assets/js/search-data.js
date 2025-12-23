9214b197944d2d1b2bd6226da4f4653633d61b29
JoseChanP <45737807+JoseChanP@users.noreply.github.com>
2025-12-23 10:39:03 +0000

    Deploying to gh-pages from @ JoseChanP/JoseChanP.github.io@38ca7dbfaeab342074e89ddd49ced76ce250789a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-projects",
+          title: "Projects",
+          description: "A collection of my work and side projects",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
